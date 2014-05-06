3e9623c88aa60a10a312d2a9f6a64d923df72caa
Júlio Santos <hi@whoisjuliosantos.com>
2014-05-06 11:57:46 +0200

    i18n

--- portalento.rb
+++ portalento.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'net/http'
+require 'json'
 
 get '/' do
+  response = Net::HTTP.get( URI.parse( "http://freegeoip.net/json/#{request.ip}" ) )
+  country_code = JSON.parse( response )["country_code"]
+
+  if (country_code.downcase == "pt")
+    redirect to "/pt"
+  else
+    redirect to "/en"
+  end
+end
+
+get '/en' do
   File.read(File.join('public', 'index.html'))
 end
+
+get '/pt' do
+  File.read(File.join('public', 'index-pt.html'))
+end
+
