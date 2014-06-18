02842b6e50b4a3ac61c33210425424b06d226acb
João Gradim <joao.gradim@gmail.com>
2014-06-18 14:58:45 +0100

    Use HAML and I18n gems for proper layouts and language logic.

--- portalento.rb
+++ portalento.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'net/http'
 require 'json'
+require 'haml'
+require 'i18n'
+require 'i18n/backend/fallbacks'
+
+configure do
+  I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+  I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+  I18n.backend.load_translations
+end
 
 get '/' do
   response = Net::HTTP.get( URI.parse( "http://freegeoip.net/json/#{request.ip}" ) )
@@ -13,11 +22,13 @@ get '/' do
   end
 end
 
-get '/en' do
-  File.read(File.join('public', 'index.html'))
+get '/:locale' do
+  I18n.locale = params[:locale]
+  haml :index
 end
 
-get '/pt' do
-  File.read(File.join('public', 'index-pt.html'))
+get '/:locale/faq' do
+  I18n.locale = params[:locale]
+  haml :faq
 end
 
